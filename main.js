2cae1ecdf92e2bd38743373e83df4788fd0e02b9
N-Mohamad <101737216+N-Mohamad@users.noreply.github.com>
2023-06-06 17:45:12 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 let rewards = ["girafe", "vélo", "mug", "télévision (cathodique)", "pc portable", "voiture thermique sans essence", "babouche usé", "un chien qui hoche la tête sur la plage arrière de la voiture", "1 million de pesos philippin (PHP)"];
 
 let btn = document.getElementById('btn');
@@ -7,7 +6,7 @@ let btnreset = document.getElementById('reset');
 let focusInput = document.getElementById('usernum');
 let focusbtn = document.getElementById('focusin');
 
-focusInput.addEventListener('keydown', function() {
+focusInput.addEventListener('keydown', function(event) {
     if (event.key === 'Enter') {
     event.preventDefault();
     validateNumber();
@@ -21,22 +20,27 @@ let maxTries = 7;
 function validateNumber(){
     let input = parseInt(focusInput.value);
     tries++;
+
     if (input === number){
         let onereward = rewards[Math.floor(Math.random() * rewards.length)];
         numlike.innerHTML = `Finally you did it the right number is ${number} you won ${onereward} `
         btn.disabled = true;
         btnreset.disabled = false;
     } else if (input < number){
-        numlike.innerHTML = `the right num is bigger than what u think `;
+        numlike.innerHTML = `the right num is bigger than what u think u still got ${maxTries - tries} attempts to go `;
     } else if (input > number) {
-        numlike.innerHTML = `the right num is lower than what u think `;
+        numlike.innerHTML = `the right num is lower than what u think u still got ${maxTries - tries} attempts to go `;
     }
-
-    if(tries === maxTries){
+    
+    if(tries >= maxTries){
         numlike.innerHTML += `you reached the maximum tries`
         btn.disabled = true
         btnreset.disabled = false;
+        focusInput.disabled = true;
     }
+    
+    focusInput.value = '';
+    
 };
 
 btn.addEventListener('click', validateNumber);
@@ -47,9 +51,12 @@ btnreset.addEventListener('click', function(){
     btn.disabled = false;
     btnreset.disabled = true;
     numlike.innerHTML = '';
-    document.getElementById('usernum').value = '';
+    focusInput.value = '';
+    
+    focusInput.disabled = false;
 });
 
 focusbtn.addEventListener ('click',() => {
     focusInput.focus();
 });
+
